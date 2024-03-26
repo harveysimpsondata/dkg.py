0c3005d03f5d5f8d5c9ffdabc120e3fee6e83b39
Lee Simpson <harveysimpsondata@gmail.com>
2024-03-25 22:28:59 -0400

    Document Parser base64 images

--- examples/document_parser.py
+++ examples/document_parser.py
@@ -5,6 +5,22 @@ from unstructured_client.models import shared
 from unstructured_client.models.errors import SDKError
 import json
 import unicodedata
+import fitz  # PyMuPDF
+import base64
+
+def extract_and_encode_images(pdf_path):
+    doc = fitz.open(pdf_path)
+    encoded_images = []  # List to store encoded images
+
+    for page in doc:
+        for img_index, img in enumerate(page.get_images(full=True)):
+            xref = img[0]
+            base_image = doc.extract_image(xref)
+            image_bytes = base_image["image"]
+            encoded_image = base64.b64encode(image_bytes).decode('utf-8')
+            encoded_images.append(encoded_image)
+
+    return encoded_images
 
 
 def normalize_text(data):
@@ -35,6 +51,8 @@ UNSTRUCTURED_URL = os.getenv('UNSTRUCTURED_URL')
 s = UnstructuredClient(api_key_auth=API_KEY, server_url=UNSTRUCTURED_URL)
 
 filename = os.path.join(os.path.dirname(__file__), '..', 'pdfs', 'Verifiable_Internet_for_Artificial_Intelligence_whitepaper.pdf')
+encoded_images = extract_and_encode_images(filename)
+
 with open(filename, "rb") as f:
 
     # Note that this currently only supports a single file
